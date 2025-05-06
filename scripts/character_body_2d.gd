085b45ec2e50274e4013fd16d0f079641f4485d8
Adam Owczarek <adam.piotr.khan@gmail.com>
2025-05-06 09:13:01 +0200

    added sound to play when jumping to simulate conflict

--- scripts/character_body_2d.gd
+++ scripts/character_body_2d.gd
@@ -5,6 +5,7 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 @onready var animation = $AnimatedSprite2D
+@onready var jump_sound = $AudioStreamPlayer2D
 
 func _ready() -> void:
 	animation.play("cheer")
@@ -20,6 +21,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jump_sound.play()
 		
 
 	# Get the input direction and handle the movement/deceleration.
@@ -33,6 +35,7 @@ func _physics_process(delta: float) -> void:
 		
 	if not is_on_floor():
 		animation.play("jump")
+		
 	elif direction:
 		animation.play("run")
 	else:
