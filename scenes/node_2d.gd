32a4a8a9eba251c27e3732bf3929383ad709567f
Piotr Ste <cyngiel@users.noreply.github.com>
2025-05-10 19:26:20 +0200

    Gazetki i prolog i outro (#12)
    * gazetki i prolog
    
    * kurde to tez co za program
    
    * i to tez
    
    * outro
    
    * Update outro.gd

---
+++ scenes/node_2d.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+@export var drzwi: Node2D
+
+func _ready():
+	var target_position = drzwi.global_position
+
+	var tween := create_tween()
+	tween.tween_property(
+		self,
+		"global_position",
+		target_position,
+		1.0  # czas trwania animacji w sekundach
+	)
---
+++ scripts/outro.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+@export var naglowek: Label
+@export var akapit: Label
+
+func _ready():
+	prosze_pokazac_artykul(4)
+
+#1 bardzo dobrze #2 srednio #3 zle #4 tramwaj nie dojechal
+func prosze_pokazac_artykul(wariant : int):
+	if(wariant == 1):
+		naglowek.text = ("Kurs bez zarzutu!")
+		akapit.text =("Jan Kowalski poprowadził dziś pierwszy tramwaj elektryczny przez Warszawę bez najmniejszych problemów. Pasażerowie chwalili spokój i pewną rękę motorniczego. Nowa era rozpoczęta z klasą!")
+	elif(wariant == 2):
+		naglowek.text = ("Postęp z drobnymi zgrzytami")
+		akapit.text = ("Pierwszy kurs tramwaju elektrycznego zakończył się z drobnymi opóźnieniami. Jan Kowalski poradził sobie nieźle, choć nie obyło się bez nerwowych momentów. Początek obiecujący, ale wymaga szlifu.")
+	elif(wariant == 3):
+		naglowek.text = ("Trudny początek – ale dotarł")
+		akapit.text =("Pierwszy kurs tramwaju elektrycznego nie obył się bez problemów. Jan Kowalski miał trudności z obsługą pojazdu – gwałtowne hamowanie, opóźnienia i nerwowa atmosfera wśród pasażerów.")
+	elif (wariant == 4):
+		naglowek.text = ("Tramwaj stanął – kurs przerwany")
+		akapit.text =("Pierwszy elektryczny tramwaj utknął na trasie. Jan Kowalski nie zapanował nad maszyną, co wywołało zamieszanie wśród pasażerów. Przejazd przerwany – przyszłość elektryki pod znakiem zapytania.")
